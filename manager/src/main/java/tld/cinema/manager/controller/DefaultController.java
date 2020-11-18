37fed4ee573b1383beb2c6e57dbf7bd258809217
Onemore <onemore04@gmail.com>
2020-11-18 20:31:43 +0700

    first commit

---
+++ manager/src/main/java/tld/cinema/manager/controller/DefaultController.java
@@ -0,0 +1,12 @@
+package tld.cinema.manager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+    @GetMapping("/timetable")
+    public String timetable() {
+        return "Hello Spring from Docker!";
+    }
+}
